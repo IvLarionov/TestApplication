8e8bd085da07808d90b6a3899098b2c347f44ee9
Ivan Larionov <xlebnica.booroop@gmail.com>
2021-08-16 12:14:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/testapplication/State.java
@@ -0,0 +1,29 @@
+package com.example.testapplication;
+
+public class State {
+
+    private String name; // название
+    private String capital;  // столица
+
+    public State(String name, String capital){
+
+        this.name=name;
+        this.capital=capital;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapital() {
+        return this.capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+}
